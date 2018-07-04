f3e17edc79f335542288b3fab25cc3f46bc2d40c
Hunt, Nicolas <nhunt@mitre.org>
2018-07-04 18:42:39 -0400

    scipy why...

--- cell_seg.py
+++ cell_seg.py
@@ -24,9 +24,10 @@ class CellSeg:
         file_path = Path(__file__)
         self.in_dir = '{}/{}'.format(str(file_path.parent), img_dir)
         self.prep_out_dir = 'prep-' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        self.seg_out_dir = 'seg' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        self.seg_out_dir = 'seg-' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         self.in_images = os.listdir(self.in_dir)
         self.preprocess = self.prep()
+        self.prep_images = os.listdir(self.prep_out_dir)
         self.thresh = 125
         self.segment = self.seg(self.prep_out_dir, self.thresh)
 
@@ -38,13 +39,12 @@ class CellSeg:
 
         if glom_orig == None:
             for i in self.in_images:
-                print(i)
+                # print(i)
                 glom_orig = skimage.io.imread(self.in_dir + '/' + i)
                 glom_hed = rgb2hed(glom_orig)[:, :, 0]
                 glom_smooth = skimage.filters.gaussian(glom_hed)
 
                 hed_file = '{}/{}_h.jpg'.format(self.prep_out_dir, i.split(".")[0])
-                # hed_file = self.out_dir + '/' i.split(".")[0] + "_h" + ".jpg"
                 scipy.misc.imsave(hed_file, glom_hed)
 
         else:
@@ -53,16 +53,34 @@ class CellSeg:
 
             return glom_smooth
 
-    def seg(self, glom_prep, thresh):
-        glom_prep = skimage.morphology.opening(glom_prep)
-        val = skimage.filters.threshold_otsu(glom_prep)
-        glom_bin = glom_prep > val
+    def seg(self, glom_prep_dir=None, thresh=None, glom_prep=None):
+        try:
+            os.stat(self.seg_out_dir)
+        except:
+            os.mkdir(self.seg_out_dir)
+        if glom_prep_dir is not None and thresh is not None:
+            for i in self.prep_images:
+                glom_hed = skimage.io.imread(glom_prep_dir + '/' + i)
+                val = skimage.filters.threshold_otsu(glom_hed)
+                glom_bin = glom_hed > val
 
-        regionLabel = skimage.measure.label(glom_bin)
-        regionProps = skimage.measure.regionprops(regionLabel)
-        glom_m1 = skimage.morphology.opening(glom_bin)
+                regionLabel = skimage.measure.label(glom_bin)
+                regionProps = skimage.measure.regionprops(regionLabel)
+                glom_m1 = skimage.morphology.opening(glom_bin)
 
-        return glom_bin
+                bin_file = '{}/{}_b.jpg'.format(self.seg_out_dir, i.split("_h")[0])
+                print(glom_bin)
+                scipy.misc.imsave(bin_file, glom_bin)
+        else:
+            glom_prep = skimage.morphology.opening(glom_prep)
+            val = skimage.filters.threshold_otsu(glom_prep)
+            glom_bin = glom_prep > val
+
+            regionLabel = skimage.measure.label(glom_bin)
+            regionProps = skimage.measure.regionprops(regionLabel)
+            glom_m1 = skimage.morphology.opening(glom_bin)
+
+            return glom_bin
 
     def display_fig(self, glom_orig, glom_prep, glom_seg):
         fig, axes = plt.subplots(ncols=3, figsize=(16, 6))
@@ -91,5 +109,5 @@ if __name__ == '__main__':
     # glom_seg = seg(glom_prep, 125)
     #
     # display_fig(glom_orig, glom_prep, glom_seg)
-    seg = CellSeg('seg_img')
+    seg = CellSeg('glom_he')
     print(seg.in_dir)
